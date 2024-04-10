01e8e4b51826a0a690fb188ff323178bf858a06b
Mark Rattle <mark.rattle@gmail.com>
2024-04-09 22:01:40 -0400

    fix content parsing and make cases for flag combinations

--- makesite.go
+++ makesite.go
@@ -27,38 +27,63 @@ func main() {
 	mlog.Info("Starting makesite...")
 	startTime := time.Now()
 
-	fileFlag := flag.String("file", "text/first-post.txt", "String representing a .txt file to read from")
-	dirFlag := flag.String("dir", "text", "String representing a directory to read from")
-	
+	// fileFlag := flag.String("file", "text/first-post.txt", "String representing a .txt file to read from")
+	// dirFlag := flag.String("dir", "text", "String representing a directory to read from")
+	fileFlag := flag.String("file", "", "String representing a .txt file to read from")
+	dirFlag := flag.String("dir", "", "String representing a directory to read from")
 	flag.Parse()
 
-	mlog.Info("Parsed flags from CLI:")
-	mlog.Info("File: %v", *fileFlag)
-	mlog.Info("Dir: %v", *dirFlag)
-
-	textFiles, _ := getTextFiles(*dirFlag)
-	
-	mlog.Info("Found %v text files in directory /%v/:", len(textFiles), *dirFlag)
-	mlog.Info(strings.Join(textFiles, ", "))
-
-	for _, file := range textFiles {
-		processTextFiles(file)
+	if *fileFlag == "" && *dirFlag == "" {
+		mlog.Warning("No file or directory specified. Defaulting to demo parse of /text/latest-post.txt.")
+		*fileFlag = "text/latest-post/latest-post.txt"
+	} else {
+		mlog.Info("Parsed flags from CLI:")
+		mlog.Info("File: %v", *fileFlag)
+		mlog.Info("Dir: %v", *dirFlag)
 	}
 
-	endTime := time.Now()
-	
-	htmlSize, _ := calcFileSizeInDirectory("pages")
+	if *dirFlag != "" {
+		textFiles, _ := getTextFiles(*dirFlag)
+		
+		mlog.Info("Found %v text files in directory /%v/:", len(textFiles), *dirFlag)
+		mlog.Info(strings.Join(textFiles, ", "))
 
-	greenAndBold := chalk.Green.NewStyle().
-		WithTextStyle(chalk.Bold).
-		Style
-	
-	// This style didn't work properly in my terminal so I just used vanilla escape codes instead.
-	// bold := chalk.Bold.NewStyle().Style
+		for _, file := range textFiles {
+			processTextFiles(file)
+		}
 
-	// This is supposed to be green but may appear differently in your terminal depending on theme settings, it seems.
-	fmt.Printf(greenAndBold("\nSuccess!") + " Generated \033[1m%v\033[0m HTML files in the pages directory.  Wrote %.1f kB in %v.\n", len(textFiles), htmlSize, endTime.Sub(startTime))
-	mlog.Info("Success! Generated %v HTML files in the pages directory. Wrote %.1f kB in %v.", len(textFiles), htmlSize, endTime.Sub(startTime))
+		endTime := time.Now()
+		
+		htmlSize, _ := calcFileSizeInDirectory("pages")
+
+		greenAndBold := chalk.Green.NewStyle().
+			WithTextStyle(chalk.Bold).
+			Style
+		
+		// This style didn't work properly in my terminal so I just used vanilla escape codes instead.
+		// bold := chalk.Bold.NewStyle().Style
+
+		// This is supposed to be green but may appear differently in your terminal depending on theme settings, it seems.
+		fmt.Printf(greenAndBold("\nSuccess!") + " Generated \033[1m%v\033[0m HTML files in the pages directory.  Wrote %.1f kB in %v.\n", len(textFiles), htmlSize, endTime.Sub(startTime))
+		mlog.Info("Success! Generated %v HTML files in the pages directory. Wrote %.1f kB in %v.", len(textFiles), htmlSize, endTime.Sub(startTime))
+	} else if *fileFlag != "" {
+		processTextFiles(*fileFlag)
+
+		endTime := time.Now()
+		
+		htmlSize, _ := calcFileSizeInDirectory("pages")
+
+		greenAndBold := chalk.Green.NewStyle().
+			WithTextStyle(chalk.Bold).
+			Style
+		
+		// This style didn't work properly in my terminal so I just used vanilla escape codes instead.
+		// bold := chalk.Bold.NewStyle().Style
+
+		// This is supposed to be green but may appear differently in your terminal depending on theme settings, it seems.
+		fmt.Printf(greenAndBold("\nSuccess!") + " Generated \033[1m1\033[0m HTML files in the pages directory.  Wrote %.1f kB in %v.\n", htmlSize, endTime.Sub(startTime))
+		mlog.Info("Success! Generated 1 HTML file in the pages directory. Wrote %.1f kB in %v.", htmlSize, endTime.Sub(startTime))
+	}
 }
 
 func getTextFiles(dir string) ([]string, error) {
@@ -72,9 +97,9 @@ func getTextFiles(dir string) ([]string, error) {
 
 		// Check if the file is a .txt file
 		if !info.IsDir() && strings.HasSuffix(info.Name(), ".txt") {
-			mlog.Info("Found text file: ", path)
+			mlog.Info("Found text file: %v", path)
 
-			mlog.Info("File name: ", info.Name())
+			mlog.Info("File name: %v", info.Name())
 
 			files = append(files, path)
 		}
@@ -122,8 +147,9 @@ func readFile(fileName string) ([]byte, error) {
 
 func parseContent(contents []byte) (string, string) {
 	// TODO: fix the parsing so that the first line is the title and the rest is the body
-	title := string(contents[0:16])
-	body := strings.TrimSpace(string(contents[16:]))
+	splitContents := strings.Split(string(contents), "\n")
+	title := string(splitContents[0])
+	body := strings.TrimSpace(strings.Join(splitContents[1:], "\n"))
 	return title, body
 }
 
